3151347a320be78550742c52a846730d12f0195e
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-07-05 10:36:53 +0200

    fix mysql username generation

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ provider "random" {
 
 resource "random_string" "mysql-admin-username" {
   length = 10
+  special = false
 }
 
 resource "random_string" "mysql-db-name" {
@@ -19,7 +20,7 @@ resource "random_string" "mysql-db-name" {
 resource "random_string" "mysql-admin-password" {
   length = 16
   special = true
-  override_special = "/@\" "
+  override_special = "/@><*&\" "
 }
 
 resource "azurerm_mysql_server" "mysql-server" {
@@ -33,12 +34,14 @@ resource "azurerm_mysql_server" "mysql-server" {
 
   sku {
     name = "B_Gen4_2"
+    # TODO externalize
     capacity = 2
     tier = "Basic"
     family = "Gen4"
   }
 
   "storage_profile" {
+    # TODO externalize
     storage_mb = 51200
     backup_retention_days = 7
     geo_redundant_backup = "Disabled"
@@ -46,6 +49,7 @@ resource "azurerm_mysql_server" "mysql-server" {
 
   ssl_enforcement = "Disabled"
 
+  # TODO upgradable
   version = "5.7"
 
   tags = "${var.tags}"
